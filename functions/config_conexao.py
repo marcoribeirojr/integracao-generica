22ef1ca9933ea433d14a7a2b8ab2e7cd56fed0e4
Marco Ribeiro Jr <contato@marcoribeirojr.com.br>
2020-05-07 12:06:17 -0300

    Feat: Criaçao arquivo de conexao com SGBD
    Criaçao do arquivo de conexao com SGBD conforme parametros informados

---
+++ functions/config_conexao.py
@@ -0,0 +1,83 @@
+import os
+
+def is_string(parametro = 1):
+    """
+        Verifica se o valor passado como parâmetro é uma string
+    """
+    if isinstance(parametro, str):
+        return True
+    
+    return False
+
+def is_number(parametro = 'a'):
+    """
+        Verifica se o valor passado como parâmetro é um inteiro
+    """
+    if isinstance(parametro, int):
+        return True
+    
+    return False
+
+def valida_dados_acesso_sgbd(**kwargs):
+    """
+        Valida dados de acesso ao SGBD
+        Parâmetros:
+            - sgdb : <class 'str'>
+            - host : <class 'str'>
+            - porta : <class 'int'>
+            - banco : <class 'str'>
+            - usuario : <class 'str'>
+            - senha : <class 'str'>
+    """
+    argumentos = [
+        'sgbd',
+        'host',
+        'porta',
+        'banco',
+        'usuario',
+        'senha'
+    ]
+    valido = True
+    for argumento in argumentos:
+        if argumento not in kwargs:
+            valido = False
+            break
+        
+        if argumento == 'porta':
+            valido = is_number(kwargs[argumento])   
+        else:                   
+            valido = is_string(kwargs[argumento])
+        
+        if not valido:
+            break
+        
+            
+    return valido
+    
+
+def cria_arquivo_configuracao(**kwargs):
+    """
+        Cria arquivo de configuração de acesso ao banco de dados
+        Parâmetros:
+            - arquivo : <class 'str'> Local e nome do arquivo
+            - sgdb : <class 'str'>
+            - host : <class 'str'>
+            - porta : <class 'int'>
+            - banco : <class 'str'>
+            - usuario : <class 'str'>
+            - senha : <class 'str'>
+    """
+    path = os.getcwd()
+    arquivo = '.env'
+    nome_arquivo = f'{path}/{arquivo}'
+    
+    for chave, valor in kwargs.items():
+        texto = f'{chave}={valor}'
+        with open(nome_arquivo, 'a') as f:
+            f.writelines(f'{texto}\n')
+                
+    return nome_arquivo
+        
+            
+    
+
---
+++ test_config_conexao.py
@@ -0,0 +1,75 @@
+import os
+
+from functions.config_conexao import *
+
+def test_is_string_deve_retornar_true_para_string():
+    texto = 'texto de teste'
+    retorno = is_string(texto)
+    assert retorno == True   
+
+def test_is_string_deve_retornar_false_para_nao_string():
+    parametro = 1
+    retorno = is_string(parametro)
+    assert retorno != True  
+
+def test_is_number_deve_retornar_true_para_integer():
+    parametro = 1
+    retorno = is_number(parametro)
+    assert retorno == True
+
+def test_is_number_deve_retornar_false_para_nao_integer():
+    parametro = 0.3
+    retorno = is_number(parametro)
+    assert retorno != True
+    
+def test_retorna_true_para_configuracao_valida():
+    retorno = valida_dados_acesso_sgbd(
+        sgbd =  'string',
+        host =  'string',
+        porta =  1,
+        banco =  'string',
+        usuario =  'string',
+        senha =  'string'
+    )
+    assert retorno == True
+
+def test_retorna_false_para_configuracao_nao_valida():
+    retorno = valida_dados_acesso_sgbd(
+        sgbd =  'string',
+        host =  'string',
+        porta =  1.2,
+        banco =  'string',
+        usuario =  'string',
+        senha =  'string'
+    )
+    assert retorno != True
+
+def test_deve_criar_dot_env_correto():
+    path = os.getcwd()
+    arquivo = '.env'
+    nome_arquivo = f'{path}/{arquivo}'
+    retorno = cria_arquivo_configuracao(
+        sgbd =  'string',
+        host =  'string',
+        porta =  1,
+        banco =  'string',
+        usuario =  'string',
+        senha =  'string'
+    )
+    dados = [
+        'sgbd=string',
+        'host=string',
+        'porta=1',
+        'banco=string',
+        'usuario=string',
+        'senha=string'
+    ]
+    valido = True
+    with open(nome_arquivo, 'r') as f:
+        for linha in f.readlines():
+            temp = linha.replace('\n', '')
+            if temp not in dados:
+                valido = False
+    os.remove(nome_arquivo)
+    assert valido == True and retorno == nome_arquivo
+    
